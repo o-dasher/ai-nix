21a5b6f1407fee6dcd3ce20297ce4fe32bbda8f6
o-dasher <88356162+o-dasher@users.noreply.github.com>
2026-04-20 19:55:48 +0000

    chore(deps): refresh flake inputs and nvfetcher sources

--- _sources/generated.nix
+++ _sources/generated.nix
@@ -19,7 +19,7 @@
     version = "0.1.0";
     src = fetchurl {
       url = "https://persistent.oaistatic.com/codex-app-prod/Codex.dmg";
-      sha256 = "sha256-dBRID+KjGV7imhrZuMIzRswlewnc3Qh8oFC6DQaNDm8=";
+      sha256 = "sha256-KBUWRcUCEEq5nDTIN25MYNlfib3Htw/Nr2788hzFAQA=";
     };
   };
   node-pty = {
